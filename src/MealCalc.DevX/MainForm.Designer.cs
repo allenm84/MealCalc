ac6e40b1dfbe18a7d4fb9e7f8742346d6757d54e
Michael Allen <michael.allen@cognex.com>
2015-03-03 13:47:07 -0600

    Initial commit of DevExpress implementation

---
+++ src/MealCalc.DevX/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace MealCalc.DevX
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+      this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+      this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+      this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+      this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+      this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+      this.bar1 = new DevExpress.XtraBars.Bar();
+      this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+      this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+      this.barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
+      ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // barManager1
+      // 
+      this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar1});
+      this.barManager1.DockControls.Add(this.barDockControlTop);
+      this.barManager1.DockControls.Add(this.barDockControlBottom);
+      this.barManager1.DockControls.Add(this.barDockControlLeft);
+      this.barManager1.DockControls.Add(this.barDockControlRight);
+      this.barManager1.Form = this;
+      this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.barButtonItem1,
+            this.barButtonItem2,
+            this.barButtonItem3});
+      this.barManager1.MaxItemId = 3;
+      // 
+      // barDockControlTop
+      // 
+      this.barDockControlTop.CausesValidation = false;
+      this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+      this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+      this.barDockControlTop.Size = new System.Drawing.Size(575, 31);
+      // 
+      // barDockControlBottom
+      // 
+      this.barDockControlBottom.CausesValidation = false;
+      this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.barDockControlBottom.Location = new System.Drawing.Point(0, 344);
+      this.barDockControlBottom.Size = new System.Drawing.Size(575, 0);
+      // 
+      // barDockControlLeft
+      // 
+      this.barDockControlLeft.CausesValidation = false;
+      this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+      this.barDockControlLeft.Location = new System.Drawing.Point(0, 31);
+      this.barDockControlLeft.Size = new System.Drawing.Size(0, 313);
+      // 
+      // barDockControlRight
+      // 
+      this.barDockControlRight.CausesValidation = false;
+      this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+      this.barDockControlRight.Location = new System.Drawing.Point(575, 31);
+      this.barDockControlRight.Size = new System.Drawing.Size(0, 313);
+      // 
+      // bar1
+      // 
+      this.bar1.BarName = "Tools";
+      this.bar1.DockCol = 0;
+      this.bar1.DockRow = 0;
+      this.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+      this.bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.barButtonItem1, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.barButtonItem2, "", true, true, true, 0, null, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.barButtonItem3, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph)});
+      this.bar1.OptionsBar.AllowQuickCustomization = false;
+      this.bar1.OptionsBar.DisableClose = true;
+      this.bar1.OptionsBar.DisableCustomization = true;
+      this.bar1.OptionsBar.DrawDragBorder = false;
+      this.bar1.OptionsBar.UseWholeRow = true;
+      this.bar1.Text = "Tools";
+      // 
+      // barButtonItem1
+      // 
+      this.barButtonItem1.Caption = "Save";
+      this.barButtonItem1.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.Glyph")));
+      this.barButtonItem1.Id = 0;
+      this.barButtonItem1.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.LargeGlyph")));
+      this.barButtonItem1.Name = "barButtonItem1";
+      // 
+      // barButtonItem2
+      // 
+      this.barButtonItem2.Caption = "Categories";
+      this.barButtonItem2.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem2.Glyph")));
+      this.barButtonItem2.Id = 1;
+      this.barButtonItem2.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem2.LargeGlyph")));
+      this.barButtonItem2.Name = "barButtonItem2";
+      // 
+      // barButtonItem3
+      // 
+      this.barButtonItem3.Caption = "Ingredients";
+      this.barButtonItem3.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem3.Glyph")));
+      this.barButtonItem3.Id = 2;
+      this.barButtonItem3.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem3.LargeGlyph")));
+      this.barButtonItem3.Name = "barButtonItem3";
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(575, 344);
+      this.Controls.Add(this.barDockControlLeft);
+      this.Controls.Add(this.barDockControlRight);
+      this.Controls.Add(this.barDockControlBottom);
+      this.Controls.Add(this.barDockControlTop);
+      this.Name = "MainForm";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private DevExpress.XtraBars.BarManager barManager1;
+    private DevExpress.XtraBars.Bar bar1;
+    private DevExpress.XtraBars.BarDockControl barDockControlTop;
+    private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+    private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+    private DevExpress.XtraBars.BarDockControl barDockControlRight;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem3;
+
+  }
+}
+
